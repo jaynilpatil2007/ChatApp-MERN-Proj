702a136d16cce5277ea5fecc32abbda15ce0e868
Jaynil Patil <jaynilpatil2007@gmail.com>
2026-03-24 16:22:28 +0530

    ready for deployment

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,7 +1,21 @@
 import dotenv from "dotenv";
+import express from "express";
 import { app } from "./app.js";
+import path from "path";
 import { connectDB } from "./db/index.js";
 
-dotenv.config({
-    path: "./.env"
-});
+dotenv.config()
+
+const PORT = process.env.PORT || 8080;
+const __dirname = path.resolve();
+
+// Make ready for deployment:
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+app.use((req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+})
