befa0336eed536c02e58a40eb800752b8fb929e9
Jaynil Patil <jaynilpatil2007@gmail.com>
2026-04-05 05:23:52 +0530

    add backend deployed link in frontend

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:8080/api",
+    baseURL: "https://syncspace-c05l.onrender.com/api",
     withCredentials: true
 })
